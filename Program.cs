eabe8c7b0721ef54c4a025ef3382d69446ea0342
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-17 14:28:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {14,54,2,5,76,23,765 };
+            foreach(var el in list)
+            {
+                if(el % 5 == 0)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
